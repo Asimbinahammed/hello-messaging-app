7a92b490524ea7739ce40fc76647ed380036c6dd
asimbinahammed <asim242370@gmail.com>
2021-12-03 20:45:19 +0530

    Refactored: into a hygenic url

--- src/main/java/com/bridgelabz/hellomessagingapp/controller/HelloWorldController.java
+++ src/main/java/com/bridgelabz/hellomessagingapp/controller/HelloWorldController.java
@@ -10,32 +10,32 @@ import org.springframework.web.bind.annotation.*;
 public class HelloWorldController {
     Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 
-    //http://localhost:8080/hello/home
-    @RequestMapping(value = {"", "/", "/home"})
+    //http://localhost:8080/hello/app
+    @RequestMapping(value = {"", "/", "/app"})
     public String sayHello() {
         return "Hello From BridgeLabz!!!";
     }
 
-    //http://localhost:8080/hello/query?name=Asim
-    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    //http://localhost:8080/hello/app?name=Asim
+    @RequestMapping(value = {"/app"}, method = RequestMethod.GET)
     public String sayHello(@RequestParam(value = "name") String name) {
         return "Hello " + name + "!";
     }
 
-    //http://localhost:8080/hello/param/Asim
-    @GetMapping("/param/{name}")
+    //http://localhost:8080/hello/app/Asim
+    @GetMapping("/app/{name}")
     public String sayHelloParam(@PathVariable String name) {
         return "Hello " + name + "!";
     }
 
-    //http://localhost:8080/hello/post
-    @PostMapping("/post")
+    //http://localhost:8080/hello/app
+    @PostMapping("/app")
     public String sayHello(@RequestBody NameDto user) {
         return "Hello " + user.getFirstName() + " " + user.getLastName() + "!";
     }
 
-    //http://localhost:8080/hello/param
-    @PutMapping("/param/{firstName}")
+    //http://localhost:8080/hello/app
+    @PutMapping("/app/{firstName}")
     public String sayHello(@PathVariable String firstName,
                            @RequestParam(value = "lastName") String lastName) {
         return "Hello " + firstName + " " + lastName + "!";
